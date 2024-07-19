84bda402645adf7c2285d3b54f6e284875b63745
Vadim <59798898+vadshi@users.noreply.github.com>
2024-07-19 19:39:38 +0300

    Update settings.py

--- Snippets/settings.py
+++ Snippets/settings.py
@@ -125,3 +125,8 @@ STATICFILES_DIRS = [
   BASE_DIR / "static"
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
