3d01a1c7163275ac9ee874271dbedcba4b1e3ba5
Vladimir Romanov <vladimirromanov@me.com>
2025-10-24 06:04:57 +0300

    ModelForm: изменение стилей

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, ValidationError
+from django.forms import ModelForm, ValidationError, Textarea, TextInput
 from .models import Snippet
 
 
@@ -7,6 +7,20 @@ class SnippetForm(ModelForm):
         model = Snippet
         # описываем поля, которые будем запролнять в форме
         fields = ['name', 'lang', 'code']
+        labels = {'name': '', 'lang': '', 'code': ''}
+        widgets = {
+            'name': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Название спиннета',
+                'style': 'max-width: 300px'
+            }),
+            'code': Textarea(attrs={
+                'placeholder': 'Код сниппета',
+                'rows': 5,
+                'class': 'input-large',
+                'style': 'width: 50% ! important; resize: vertical !important;'
+            })
+        }
 
     def clean_name(self):
         """Метод для проверки длины поля <name>"""
